4e09e1ff1ce7b753d119d49bc2392acec75423b4
3ricwebdev <ericxdd@yandex.ru>
2024-10-19 00:46:51 +0300

    initial commit

---
+++ src/app/components/slider/index.jsx
@@ -0,0 +1,38 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import css from "./index.module.css";
+const Slider = ({ slides }) => {
+  return (
+    <Swiper
+      style={{
+        maxWidth: "500px",
+        height: "500px",
+      }}
+      modules={[Pagination, Navigation]}
+      pagination={{
+        type: "progressbar",
+      }}
+      navigation={true}
+    >
+      {slides.map((el, i) => {
+        return (
+          <SwiperSlide>
+            <img
+              src={el.src}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+export default Slider;
